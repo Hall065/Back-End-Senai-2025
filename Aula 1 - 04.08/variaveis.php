444f4cfebe4c5a53f89673337cc51410534988a8
Hall <hall065.2022@gmail.com>
2025-08-04 16:48:02 -0300

    Aula 01 - 04.08.2025

---
+++ Aula 1 - 04.08/variaveis.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo"Olá! \n";
+    $nome = "Hall! \n";
+    $idade = "21";
+    $ano_atual = 2025;
+
+    $data_nasc= $ano_atual - $idade;
+    echo "Data de nascimento: $data_nasc \n";
+
+?>
